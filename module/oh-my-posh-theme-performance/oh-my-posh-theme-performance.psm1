8a83e425b45dc0afae9ee5f3b8228c9c59d10baa
Lifailon <deadepicboss@gmail.com>
2024-02-08 13:01:25 +0300

    Update module

--- module/oh-my-posh-theme-performance/oh-my-posh-theme-performance.psm1
+++ module/oh-my-posh-theme-performance/oh-my-posh-theme-performance.psm1
@@ -1,6 +1,13 @@
 function Set-PoshTheme {
     param (
-        $Theme
+        [Parameter(Mandatory)][ValidateSet(
+            "System-Sensors",
+            "System-Performance",
+            "Pwsh-Process-Performance"
+        )]$Theme,
+        [switch]$Save
     )
-    
+    Invoke-Expression $(
+        Invoke-RestMethod https://raw.githubusercontent.com/Lifailon/oh-my-posh-themes-performance/rsa/themes/$Theme.ps1
+    )
 }
