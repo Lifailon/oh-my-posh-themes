7493768c1e4341b3c1e590027916dd198145d670
Lifailon <deadepicboss@gmail.com>
2024-02-08 13:35:49 +0300

    Update module

--- module/oh-my-posh-theme-performance/oh-my-posh-theme-performance.psm1
+++ module/oh-my-posh-theme-performance/oh-my-posh-theme-performance.psm1
@@ -10,4 +10,12 @@ function Set-PoshTheme {
     Invoke-Expression $(
         Invoke-RestMethod https://raw.githubusercontent.com/Lifailon/oh-my-posh-themes-performance/rsa/themes/$Theme.ps1
     )
+    if ($Save) {
+        $Path = "$home\AppData\Local\Programs\oh-my-posh\themes\$theme.omp.json"
+        $ThemeJson   = $(Invoke-WebRequest https://raw.githubusercontent.com/Lifailon/oh-my-posh-themes-performance/rsa/themes/$Theme.omp.json).Content
+        $ThemeJson   | Out-File $Path
+        $ThemeScript = $(Invoke-WebRequest https://raw.githubusercontent.com/Lifailon/oh-my-posh-themes-performance/rsa/themes/$Theme.ps1).Content
+        $ThemeScript = $ThemeScript -replace 'oh-my-posh init pwsh --config ".+"',"oh-my-posh init pwsh --config '$Path'"
+        $ThemeScript | Out-File $Profile
+    }
 }
