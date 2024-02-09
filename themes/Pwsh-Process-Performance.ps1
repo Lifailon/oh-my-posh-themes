ffc3e7222d106cd79f1cd4b7d65150ca453cf8ec
Lifailon <deadepicboss@gmail.com>
2024-02-09 11:25:25 +0300

    Add jobj to theme pwsh-process-performance

--- themes/Pwsh-Process-Performance.ps1
+++ themes/Pwsh-Process-Performance.ps1
@@ -5,6 +5,10 @@ function Global:Set-EnvVariable {
     $env:POSH_MEM_USE = $($($GetProcess | Where-Object id -eq $($env:POSH_PID)).WorkingSet / 1mb).ToString("0")
     $env:POSH_MEM_USE_ALL = $($($GetProcess | Measure-Object WorkingSet -sum).sum / 1mb).ToString("0")
     $env:POSH_PROCESS_COUNT = $GetProcess.Count
+    $env:POSH_JOB_COUNT = $(Get-Job).Count
+    $env:POSH_JOB_RUNNING = $(Get-Job -State Running).Count
+    $env:POSH_JOB_COMPLETED = $(Get-Job -State Completed).Count
+    $env:POSH_JOB_FAILED = $(Get-Job -State Failed).Count
 }
 New-Alias -Name "Set-PoshContext" -Value "Set-EnvVariable" -Scope Global -Force
 oh-my-posh init pwsh --config "https://raw.githubusercontent.com/Lifailon/oh-my-posh-themes/rsa/themes/Pwsh-Process-Performance.omp.json" | Invoke-Expression
